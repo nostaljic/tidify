4540f431c034b373a7b54d4e715459cc90c6441a
Scorpion <scorpion@dgu.ac.kr>
2022-08-24 16:39:53 +0900

    feat: Add folder presentaiton layer

--- src/router/router.go
+++ src/router/router.go
@@ -1,11 +1,32 @@
 package router
 
 import (
+	"log"
+	"tidify/interactor"
+	"tidify/repository"
+
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
+//SetupRoutes : all the routes are defined here
 func SetupRoutes(db *gorm.DB) {
 	httpRouter := gin.Default()
+
+	folderRepository := repository.NewFolderRepository(db)
+	if err := folderRepository.Migrate(); err != nil {
+		log.Fatal("Folder migrate err", err)
+	}
+	folderGroup := httpRouter.Group("/folders")
+	folderInteractor := &interactor.FolderInteractor{FolderRepository: folderRepository}
+	setUpFolder(folderGroup, folderInteractor)
+
 	httpRouter.Run(":8888")
+
+}
+func setUpFolder(group *gin.RouterGroup,
+	folderInteractor *interactor.FolderInteractor,
+) {
+	group.GET("", folderInteractor.GetFolder)
+	group.POST("", folderInteractor.CreateFolder)
 }
